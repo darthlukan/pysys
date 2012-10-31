29cd12ca7906a185b9a47d3e8d1266b8b539165e
Brian Tomlinson <darthlukan@gmail.com>
2012-10-30 18:08:23 -0600

    added some more to the basic structure, using dicts, missing logic in backup_copy, need to refactor initial command calls, need to test

--- syscleanup/BetterBackup.py
+++ syscleanup/BetterBackup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Filename: BetterBackup.py                                                   #
 # Author: Brian Tomlinson <darthlukan@gmail.com>                              #
@@ -9,37 +9,109 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import glob
 import shutil
-
-# File Type Lists, audio, video, images, documents, archives
-audlst = ['*.mp3', '*.ogg', '*.wav']
-
-vidlst = ['*.mpg', '*.mp4', '*.mkv',
-          '*.avi', '*.mpeg', '*.mpg', '*.flv']
-
-imglst = ['*.png', '*.jpg', '*.jpeg',
-          '*.gif', '*.raw', '*.svg']
-
-doclst = ['*.doc', '*.docx', '*.txt',
-          '*.log', '*.odt', '*.org',
-          '*.xls', '*.xlsx', '*.ods']
-
-archlst = ['*.rar', '*.zip', '*.tar',
-            '*.gz', '*.tgz', '*.bz2',
-            '*.tbz2', '*.7z', '*.s7z']
-
-typeslist = [audlst, vidlst, imglst, doclst, archlst]
 
 # Get the user and their $HOME
 userhome = '/home/' + os.getlogin()
 
-# Hard-coding the expected backup paths, user interaction based pathing
-# will happen later.
-dirlist = [userhome + '/backups/archives', userhome + '/backups/images',
-           userhome + '/backups/videos', userhome + '/backups/docs',
-           userhome + '/backups/music', userhome + '/backups/misc']
+# Set our supported formats.
+formats = {
+    'audio': ['*.mp3', '*.ogg', '*.wav'],
+    'video': ['*.mpg', '*.mp4', '*.mkv',
+              '*.avi', '*.mpeg', '*.mpg',
+              '*.flv'],
+    'image': ['*.png', '*.jpg', '*.jpeg',
+              '*.gif', '*.raw', '*.svg'],
+    'docs': ['*.doc', '*.docx', '*.txt',
+             '*.log', '*.odt', '*.org',
+             '*.xls', '*.xlsx', '*.ods'],
+    'archives': ['*.rar', '*.zip', '*.tar',
+                 '*.gz', '*.tgz', '*.bz2',
+                 '*.tbz2', '*.7z', '*.s7z']
+    }
+
+# Set the directories that we expect
+dirs = {
+    'home': userhome,
+    'audio': userhome + '/backups/music',
+    'video': userhome + '/backups/videos',
+    'image': userhome + '/backups/images',
+    'docs': userhome + '/backups/docs',
+    'archives': userhome + '/backups/archives',
+    'misc': userhome + '/backups/misc'
+    }
 
 # General backup function, WIP
-def backup_copy():
-    print "TODO"
+def backup_copy(op):
+    # Placeholder
+    print('Backing up')
+    
+def check_dirs(op):
+    for key, val in dirs.items():
+        if key == op:
+            if os.path.isdir(val):
+                backup_copy(op)
+            else:
+                print("That directory does not exist, would you like to create it?")
+                answer = input("Y/n: ")
+                if answer.lower() == 'y':
+                    create_path(op)
+                elif answer.lower() == 'n':
+                    quitter()
+                else:
+                    sys.exit('Invalid input entered, exiting.')
+        else:
+            print('Somthing bad happened, exiting.')
+            sys.exit('Logic error encountered.')
+
+# Callers    
+def every():
+    check_dirs('misc')
+    
+def pics():
+    check_dirs('image')
+    
+def vids():
+    check_dirs('video')
+    
+def audio():
+    check_dirs('audio')
+    
+def docs():
+    check_dirs('docs')
+
+def archs():
+    check_dirs('archives')
+
+def quitter():
+    sys.exit('Exited because of user input.')
+    
+def main():
+    commands = {
+            'ALL': every,
+            'QUIT': quitter,
+            'PICTURES': pics,
+            'VIDEOS': vids,
+            'MUSIC': audio,
+            'DOCUMENTS': docs,
+            'ARCHIVES': archs
+            }
+        
+    while true:
+        for key in commands:
+            print(key)
+        user_input = input('What would you like to do?: ')
+        
+        arg = user_input.lower()
+        
+        if arg in commands.keys():
+            commands[arg]()
+        else:
+            print("%s not a recognized command." % (arg))
+            sys.exit('Exiting')                
+                
+
+if __name__ == '__main__':
+    main()
