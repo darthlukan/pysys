67929824bc30852216277e03a67f0c8d1d3aeefb
Brian Tomlinson <brian@smartm.no>
2013-09-24 16:18:26 +0200

    Changed formatting to py3

--- filescripts/logreader.py
+++ filescripts/logreader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Filename: logreader.py
 # Author: Brian Tomlinson
@@ -18,12 +18,14 @@ info = []
 # Errors specific to PDF Conversion, NYI
 pdfConvErrs = []
 
+
 def check_lines(logObj):
     for line in logObj:
         line.rstrip('\r\n')
         everyLine.append(line)
     logObj.close()
     return everyLine
+
 
 def sort_lines():
     for i in everyLine:
@@ -36,45 +38,51 @@ def sort_lines():
         else:
             continue
 
-    print 'Populated lists:'
-    print ''
+    print('Populated lists:')
+    print('')
+
     if len(debug) > 0:
-        print 'Debug'
+        print('Debug')
 
     if len(error) > 0:
-        print 'Error'
+        print('Error')
 
     if len(info) > 0:
-        print 'Info'
-    print ''
+        print('Info')
+    print('')
+
 
 def display_info():
     for i in info:
-        print i
+        print(i)
     get_tasks()
+
 
 def display_error():
     for i in error:
-        print i
+        print(i)
     get_tasks()
+
 
 def display_debug():
     for i in debug:
-        print i
+        print(i)
     get_tasks()
+
 
 def display_all():
     for i in everyLine:
-        print i
+        print(i)
     get_tasks()
+
 
 def output_to_file_write(copyList):
     task = copyList
-    if os.path.exists('/tmp/logreader_out.log') == False:
+    if not os.path.exists('/tmp/logreader_out.log'):
         outputFile = open('/tmp/logreader_out.log', 'w')
     else:
         outputFile = open('/tmp/logreader_out.log', 'a')
-    print 'Outputting requested information to /tmp/logreader.log'
+    print('Outputting requested information to /tmp/logreader.log')
     if task == 1:
         for i in debug:
             outputFile.writelines(i + '\n')
@@ -88,36 +96,38 @@ def output_to_file_write(copyList):
         for i in everyLine:
             outputFile.writelines(i + '\n')
     else:
-        print 'Input was invalid, returning you to the previous menu.'
+        print('Input was invalid, returning you to the previous menu.')
         output_to_file_select()
     outputFile.flush()
     outputFile.close()
-    print 'Requested operation completed successfully.'
+    print('Requested operation completed successfully.')
     output_to_file_select()
 
+
 def output_to_file_select():
-    print 'Please select the information to copy to a file:'
-    print ''
-    print '1: Debug messages'
-    print '2: Error messages'
-    print '3: Info messages'
-    print '4: All'
-    print '0: Return to main menu'
+    print('Please select the information to copy to a file:')
+    print('')
+    print('1: Debug messages')
+    print('2: Error messages')
+    print('3: Info messages')
+    print('4: All')
+    print('0: Return to main menu')
     copyList = input('Enter your choice: ')
     if copyList != 0:
         output_to_file_write(copyList)
     else:
-        print 'Returning you to the main menu.'
+        print('Returning you to the main menu.')
         get_tasks()
 
+
 def get_tasks():
-    print 'What would you like to display?'
-    print '1: Debug messages'
-    print '2: Error messages'
-    print '3: Info messages'
-    print '4: All (Messy)'
-    print '5: Output to file'
-    print '0: Exit'
+    print('What would you like to display?')
+    print('1: Debug messages')
+    print('2: Error messages')
+    print('3: Info messages')
+    print('4: All (Messy)')
+    print('5: Output to file')
+    print('0: Exit')
     task = input('Enter your choice: ')
 
     if task == 1:
@@ -135,10 +145,12 @@ def get_tasks():
     else:
         sys.exit('Invalid input, please try again using only 1, 2, 3, 4, or 0.')
 
+
 def main(logObj):
     check_lines(logObj)
     sort_lines()
     get_tasks()
+
 
 if __name__ == '__main__':
     logObj = open(sys.argv[1], 'r')
