46880b3a4f7bb410485818d7334d22f8e9876bf5
Brian Tomlinson <darthlukan@gmail.com>
2015-06-17 21:11:14 +0200

    minor cleanup, remove print

--- courses/edX/MITx/600x/sqroot_recipe.py
+++ courses/edX/MITx/600x/sqroot_recipe.py
@@ -15,17 +15,17 @@ def get_find(find):
 
 
 def start_guessing(find, guess, c_guess, counter):
-    
+
     if counter > 0:
         counter = counter
     else:
         counter = 0
-        
+
     if c_guess > 1:
         start = c_guess
     else:
         start = 1
-    
+
     for i in range(start, find):
         c_guess += i
         if (i * i) == find:
@@ -38,7 +38,6 @@ def start_guessing(find, guess, c_guess, counter):
                 guess = "FAILED!"
                 return guess
             else:
-                print "Running again, current counter = %d" % counter
                 start_guessing(find, guess, c_guess, counter)
     return guess
 
@@ -47,16 +46,16 @@ def main():
 
     guess = 0
     c_guess = 0
+
     find = 0
-    
-    find = get_find(find)    
-    
+    find = get_find(find)
+
     if test_find_over_zero(find):
         final_guess = start_guessing(find, guess, c_guess, counter=0)
         response = "Our best guess is %d" % final_guess
         return response
     else:
         find = get_find(find)
-        
+
 if __name__ == '__main__':
     main()
